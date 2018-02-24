bb106dbaaac8633e390253951382158b0417b528
David Luciano Gonçalves Lourenço <david.lourenco11@gmail.com>
2018-02-24 19:47:03 +0000

    Organizing Code

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
-    $(".menu_pomodoro").click(function () {
-        $(".center_action").css("background-color", "red");
-    });
-    $(".menu_timer").click(function () {
-        $(".center_action").css("background-color", "green");
-    });
-    $(".menu_countdown").click(function () {
-        $(".center_action").css("background-color", "blue");
-    });
-}); 
+$(document).ready(function() {
+  $(".menu_pomodoro").click(function() {
+    $(".center_action").css("background-color", "red");
+  });
+  $(".menu_timer").click(function() {
+    $(".center_action").css("background-color", "green");
+  });
+  $(".menu_countdown").click(function() {
+    $(".center_action").css("background-color", "blue");
+  });
+});
