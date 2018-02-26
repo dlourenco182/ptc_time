5faa7509f9b0a0f5695e3e843f2a15b6246e4721
David Luciano Gonçalves Lourenço <david.lourenco11@gmail.com>
2018-02-26 21:21:53 +0000

    Added the local time feature
    One of the time I want to show is the real time for the viewer

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-$(document).ready(function() {
-  $(".menu_pomodoro").click(function() {
+$(document).ready(function () {
+
+  var myVar = setInterval(myTimer, 1000);
+
+  function myTimer() {
+    var d = new Date();
+    document.getElementById("time_local").innerHTML = d.toLocaleTimeString();
+  }
+
+  $(".menu_pomodoro").click(function () {
     $(".center_action").css("background-color", "red");
   });
-  $(".menu_timer").click(function() {
+  $(".menu_timer").click(function () {
     $(".center_action").css("background-color", "green");
   });
-  $(".menu_countdown").click(function() {
+  $(".menu_countdown").click(function () {
     $(".center_action").css("background-color", "blue");
   });
 });
