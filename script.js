95aebbd9fd3c3f4874f3143b617c33cc524fe185
David Luciano Gonçalves Lourenço <david.lourenco11@gmail.com>
2018-02-28 23:42:40 +0000

    Progress Bar + Notifications
    Progress Bar adjust with the time  and Notifications now outsisde of the browser

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
 
-  var something = setInterval(progressbar_timer,1000);
+
+
   $(".form_timer").hide();
   var myVar = setInterval(myTimer, 1000);
   var hoursLabel = document.getElementById("hours");
   var minutesLabel = document.getElementById("minutes");
   var secondsLabel = document.getElementById("seconds");
   var totalSeconds = 0;
-  var progressbar_timer = null;
   var myTimer = null;
 
   function myTimer() {
@@ -15,22 +15,21 @@ $(document).ready(function () {
     document.getElementById("time_local").innerHTML = d.toLocaleTimeString();
   }
 
-  function progressbar_timer() {
-    var progress = Math.round($(".progress-bar-striped.active").width());
-    progress = Math.round((progress+100)/10)+'%';
-    if(progress ==100){clearInterval(progressbar_timer);}else{
-    $(".progress-bar-striped").css("width",progress);
-    $(".progress-bar-striped")[0].innerHTML = progress;}
-  }
+  /*  function progressbar_timer() {
+      $(".progress-bar-striped").css("width", "100%");
+      $(".progress-bar-striped")[0].innerHTML = "100%";
+
+    }*/
 
   $("#start_button").click(function () {
 
     if (jQuery.inArray("active", $('.menu_timer')[0].classList) > 0) {
-
-      var hours = 6;
-      var minutes = 6;
-      var seconds = 6;
-
+      $(".progress-bar-striped").css("width", "0%");
+      var hours = 0;
+      var minutes = 0;
+      var seconds = 30;
+      var time_progress_ml = (hours * 60 * 60 * 1000) + (minutes * 60 * 1000) + (seconds * 1000)
+      alert(time_progress_ml);
       myTimer = setInterval(setTimer, 1000);
 
       function setTimer() {
@@ -38,7 +37,16 @@ $(document).ready(function () {
         seconds--;
 
         if (hours == 0 && minutes == 0 && seconds == 0) {
-          alert('success');
+          var displayNotification = function(title, message){
+            // detect what type of notification should be
+            // used (WebNotification or a simple alert)
+            
+              new Notification(title, {
+                icon : "notification.png",
+                body : message
+              });
+          };
+          displayNotification("ALERT", "Your timer is finished!");
         }
 
         if (seconds < 0) {
@@ -67,8 +75,9 @@ $(document).ready(function () {
         document.getElementById("hours").innerHTML = hours;
 
       }
-
-
+      $(".progress-bar-striped").animate({
+        width: '100%'
+      }, time_progress_ml);
 
 
     } else {
